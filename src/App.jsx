6bdb2b4a5fad7fcd09cc9f6c2c75fb3e2de97c24
Amit-Yadav21 <amit21navgurukul.org>
2024-06-25 16:23:42 +0530

    Again ressponsive

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,46 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
-import './App.css'
+import './App.css';
 
-import UperNavBar from './components/UperNavBar/UperNavBar'
+import React, { useState, useEffect } from 'react';
+import UperNavBar from './components/UperNavBar/UperNavBar';
 import NavBar from './components/NavBar/NavBar';
 import Banner from './components/Banner/Banner';
 import WelcomCakezone from './components/WelcomCakeZone/WelcomCakezone';
 import Number from './components/Number/Number';
-import MenuPricing from './components/MenuPricing/MenuPricing'
+import MenuPricing from './components/MenuPricing/MenuPricing';
 import DiscountSummer from './components/DiscountSummer/DiscountSummer';
-import OurMasterChafes from './components/OurMasterChefs/OurMasterChefs'
-import SuperCrispyCakes from './components/SuperCrispyCakes/SuperCrispyCakes'
-import Testimonial from './components/Testimonial/Testimonial'
+import OurMasterChafes from './components/OurMasterChefs/OurMasterChefs';
+import SuperCrispyCakes from './components/SuperCrispyCakes/SuperCrispyCakes';
+import Testimonial from './components/Testimonial/Testimonial';
 import Footer from './components/Footer/Footer';
 
 function App() {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 768);
+    };
+
+    handleResize();
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
   return (
     <>
-      <UperNavBar />
+      {!isMobile && <UperNavBar />}
       <NavBar />
       <Banner />
       <WelcomCakezone />
       <Number />
-      <MenuPricing/>
-      <DiscountSummer/>
+      <MenuPricing />
+      <DiscountSummer />
       <OurMasterChafes />
       <SuperCrispyCakes />
-      <Testimonial/>
-      <Footer/>
+      <Testimonial />
+      <Footer />
     </>
   );
 }
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -4,14 +4,15 @@ function NavBar() {
     return (
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
             <div className="container-fluid">
-                <a className="navbar-brand text-white fw-bold" href="#">CAKEZONE</a>
+                <i className="fas fa-cake-candles fa-3x" style={{ color :'#FFA500', marginRight:'5px'}}></i>
+                <a className="navbar-brand text-white fw-bold mr-2" href="#">CAKEZONE</a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse justify-content-center" id="navbarSupportedContent">
                     <ul className="navbar-nav mb-2 mb-lg-0">
                         <li className="nav-item">
-                            <a className="nav-link active fw-bold" aria-current="page" href="#" style={{color:'orange'}}>Home</a>
+                            <a className="nav-link active fw-bold" aria-current="page" href="#" style={{ color: 'orange' }}>Home</a>
                         </li>
                         <li className="nav-item">
                             <a className="nav-link text-white fw-bold" href="#">ABOUT US</a>
